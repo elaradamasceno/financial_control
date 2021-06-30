1e9ce024581778b5f9f65bd77e7d402fd0c7c117
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-06-30 16:07:59 -0300

    input and output filters - resolved

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -29,7 +29,10 @@ interface IData{
 }
 
 const List: React.FC<IRouteParams> = ({ match }) => {
-  const [ data, setData ] = useState<IData[]>([]);
+  const [data, setData] = useState<IData[]>([]);
+  const [currentMonth, setCurrentMonth] = useState<string>(String(new Date().getMonth() + 1));
+  const [currentYear, setCurrentYear] = useState<string>(String(new Date().getFullYear()));
+
   const { type } = match.params;
 
   const title = useMemo(() => {
@@ -41,19 +44,29 @@ const List: React.FC<IRouteParams> = ({ match }) => {
   }, [type])
 
   const months = [
-    { value: 6, label: 'Junho'},
+    { value: 8, label: 'Agosto'},
     { value: 7, label: 'Julho'},
-    { value: 8, label: 'Agosto'}
+    { value: 6, label: 'Junho'},
+    { value: 1, label: 'Janeiro'},
+    { value: 3, label: 'Março'},
   ];
 
   const years = [
+    { value: 2019, label: 2019 },
     { value: 2021, label: 2021 },
     { value: 2020, label: 2020 },
-    { value: 2019, label: 2019 }
   ];
 
   useEffect(() => {
-    let resp = listData.map((item) => {
+    let filteredDate = listData.filter((item) => {
+      const date = new Date(item.date);
+      const filterMonth = String(date.getMonth() + 1);  
+      const filterYear = String(date.getFullYear());
+      
+      return filterMonth === currentMonth && filterYear === currentYear;
+    });
+
+    let resp = filteredDate.map((item) => {
       return{
         description: item.description,
         amountFormatted: formatCurrency(Number(item.amount)),
@@ -61,16 +74,29 @@ const List: React.FC<IRouteParams> = ({ match }) => {
         dateFormatted: formatDate(item.date),
         tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
       }
-    })
+    });
 
     setData(resp);
-  }, [])
+  }, [listData, currentMonth, currentYear])
   
   return(
     <Container>
       <ContentHeader title={title.title} lineColor={title.lineColor}>
-        <SelectInput options={months}/>
-        <SelectInput options={years}/>
+        <SelectInput 
+          options={months} 
+          onChange={
+            (e) => setCurrentMonth(e.target.value)
+          } 
+          defaultValue={currentMonth}
+        />
+
+        <SelectInput 
+          options={years} 
+          onChange={
+            (e) => setCurrentYear(e.target.value)
+          } 
+          defaultValue={currentYear}
+        />
       </ContentHeader>
 
       <Filters>
